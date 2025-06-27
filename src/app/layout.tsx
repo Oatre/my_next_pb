566ecafa13a9cb911fddee56977c88045055dafe
Oatre <jensenandfur@gmail.com>
2025-06-27 14:08:14 +0200

    Commented out fonts to fix deployment issues with Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+/*const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -11,10 +11,14 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
-});
+});*/
+/* Removing fonts for deploymet issues with Vercel, keeping them in for future reference */
+
+
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
